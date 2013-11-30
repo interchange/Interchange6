b07c32acfabe986ee76d9156a575ec614379bd51
Stefan Hornburg (Racke) <racke@linuxia.de>
2013-11-30 16:43:27 +0100

    Add Interchange6 module and infrastructure.

---
+++ lib/Interchange6.pm
@@ -0,0 +1,58 @@
+package Interchange6;
+
+=head1 NAME
+
+Interchange6 - Open Source Shop Machine
+
+=head1 VERSION
+
+0.001
+
+=cut
+
+our $VERSION = '0.001';
+
+=head1 DESCRIPTION
+
+Interchange6, the Open Source Shop Machine, is the Modern Perl ecosystem
+for online business.
+
+This module provides the following APIs:
+
+=over 4
+
+=item Carts
+
+L<Interchange6::Cart>
+
+=back
+
+To build your own business website, please take a look at
+our Dancer plugin: L<Dancer::Plugin::Interchange6>.
+
+=head1 CART
+
+Interchange6 supports multiple carts, automatic collapsing of similar items
+and price caching.
+
+=head1 CAVEATS
+
+Please anticipate API changes in this early state of development.
+
+=head1 AUTHOR
+
+Stefan Hornburg (Racke), C<racke@linuxia.de>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011-2013 Stefan Hornburg (Racke).
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1;
