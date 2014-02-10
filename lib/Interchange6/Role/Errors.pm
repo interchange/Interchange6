d8b56d310b5745b594c01df1115c8b9c719f275a
Peter Mottram <peter@sysnix.com>
2014-02-10 15:02:38 +0100

    renamed all method to errors in Role::Errors

--- lib/Interchange6/Role/Errors.pm
+++ lib/Interchange6/Role/Errors.pm
@@ -4,20 +4,12 @@ use MooX::HandlesVia;
 use Interchange6::Types;
 
 # The errors registry
-<<<<<<< HEAD
 has _errors => (
-=======
-has errors => (
->>>>>>> add new Errors role
     is          => 'rw',
     default     => sub { [] },
     handles_via => 'Array',
     handles => {
-<<<<<<< HEAD
         errors       => 'all',
-=======
-        all          => 'all',
->>>>>>> add new Errors role
         has_error    => 'count',
         has_errors   => 'count',
         clear_error  => 'clear',
@@ -28,23 +20,11 @@ has errors => (
 
 sub get_error {
     my $self = shift;
-<<<<<<< HEAD
     return join(':', $self->errors);
 };
 sub error {
     my $self = shift;
     return join(':', $self->errors);
-=======
-    return join(':', $self->all);
-};
-sub get_errors {
-    my $self = shift;
-    return join(':', $self->all);
-};
-sub error {
-    my $self = shift;
-    return join(':', $self->all);
->>>>>>> add new Errors role
 };
 
 1;
